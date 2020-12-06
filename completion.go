fc6271e2bef75a49d2baa10a026066a499c9bf70
rick <rick@jenkins-zh.cn>
2020-12-06 22:34:01 +0800

    Add completion cmd

---
+++ completion.go
@@ -0,0 +1,74 @@
+package pkg
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+// CompletionOptions is the option of completion command
+type CompletionOptions struct {
+	Type string
+}
+
+// ShellTypes contains all types of shell
+var ShellTypes = []string{
+	"zsh", "bash", "powerShell",
+}
+
+var completionOptions CompletionOptions
+
+func NewCompletionCmd(rootCmd *cobra.Command) (cmd *cobra.Command) {
+	rootName := rootCmd.Name()
+
+	cmd = &cobra.Command{
+		Use:   "completion",
+		Short: "Generate shell completion scripts",
+		Long: fmt.Sprintf(`Generate shell completion scripts
+Normally you don't need to do more extra work to have this feature if you've installed %s by brew`, rootName),
+		Example: fmt.Sprintf(`Installing bash completion on macOS using homebrew
+If running Bash 3.2 included with macOS
+brew install bash-completion
+or, if running Bash 4.1+
+brew install bash-completion@2
+You may need to add the completion to your completion directory by the following command
+%s completion > $(brew --prefix)/etc/bash_completion.d/%s
+If you get trouble, please visit https://github.com/jenkins-zh/jenkins-cli/issues/83.
+
+In order to have good experience on zsh completion, ohmyzsh is a good choice.
+Please install ohmyzsh by the following command
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+Get more details about onmyzsh from https://github.com/ohmyzsh/ohmyzsh
+
+Load the %s completion code for zsh[1] into the current shell
+source <(%s completion --type zsh)
+Set the %s completion code for zsh[1] to autoload on startup
+%s completion --type zsh > "${fpath[1]}/_%s"`, rootName),
+		RunE: func(cmd *cobra.Command, _ []string) (err error) {
+			shellType := completionOptions.Type
+			switch shellType {
+			case "zsh":
+				err = rootCmd.GenZshCompletion(cmd.OutOrStdout())
+			case "powerShell":
+				err = rootCmd.GenPowerShellCompletion(cmd.OutOrStdout())
+			case "bash":
+				err = rootCmd.GenBashCompletion(cmd.OutOrStdout())
+			default:
+				err = fmt.Errorf("unknown shell type %s", shellType)
+			}
+			return
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVarP(&completionOptions.Type, "type", "", "bash",
+		fmt.Sprintf("Generate different types of shell which are %v", ShellTypes))
+
+	err := cmd.RegisterFlagCompletionFunc("type", func(cmd *cobra.Command, args []string, toComplete string) (
+		i []string, directive cobra.ShellCompDirective) {
+		return ShellTypes, cobra.ShellCompDirectiveDefault
+	})
+	if err != nil {
+		cmd.PrintErrf("register flag type for sub-command doc failed %#v\n", err)
+	}
+	return
+}
